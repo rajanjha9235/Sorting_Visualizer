3e947a2fa98e9c16b17df1f4c23f5165ed17a8d8
Rajan Jha <138964806+rajanjha9235@users.noreply.github.com>
2024-12-23 19:29:04 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,14 @@
 function Footer() {
   const currentYear = new Date().getFullYear();
   return (
-    <footer className="flex justify-between items-center mt-8 py-4 border-t bg-gray-800 text-white">
+    <footer className="w-full flex flex-col sm:flex-row justify-between items-center mt-8 py-4 border-t bg-gray-800 text-white px-4">
       <p>&copy; {currentYear} Rajan Jha</p>
-      <div className="flex">
+      <div className="flex mt-4 sm:mt-0">
         <a href="https://github.com/rajanjha9235/Sorting_Visualizer" target="_blank" rel="noopener noreferrer">
-          <img src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png" alt="GitHub" style={{ width: '24px', height: '24px' }} />
+          <img src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png" alt="GitHub" className="w-12 h-12 rounded-full" />
         </a>
-        <a href="https://www.linkedin.com/in/rajanjha9235" target="_blank" rel="noopener noreferrer">
-          <img src="https://upload.wikimedia.org/wikipedia/commons/8/81/LinkedIn_icon.svg" alt="LinkedIn" style={{ width: '24px', height: '24px', marginLeft: '10px' }} />
+        <a href="https://www.linkedin.com/in/rajanjha9235" target="_blank" rel="noopener noreferrer" className="ml-4">
+          <img src="https://upload.wikimedia.org/wikipedia/commons/8/81/LinkedIn_icon.svg" alt="LinkedIn" className="w-12 h-12 rounded-full" />
         </a>
       </div>
     </footer>
