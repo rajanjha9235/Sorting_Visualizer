fb6027c040eda71816ece1e6658a2628d597e751
Rajan Jha <138964806+rajanjha9235@users.noreply.github.com>
2024-12-23 18:56:47 +0530

    Create Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,17 @@
+function Footer() {
+  return (
+    <footer className="text-center mt-8 py-4 border-t">
+      <p>&copy; 2024 Rajan Jha</p>
+      <p>
+        <a href="https://github.com/rajanjha9235/Sorting_Visualizer" target="_blank" rel="noopener noreferrer">
+          <img src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png" alt="GitHub" style={{ width: '24px', height: '24px' }} />
+        </a>
+        <a href="https://www.linkedin.com/in/rajanjha9235" target="_blank" rel="noopener noreferrer">
+          <img src="https://upload.wikimedia.org/wikipedia/commons/8/81/LinkedIn_icon.svg" alt="LinkedIn" style={{ width: '24px', height: '24px', marginLeft: '10px' }} />
+        </a>
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
