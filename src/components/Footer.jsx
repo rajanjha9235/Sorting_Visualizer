996f54390d5ffc9992298d6a9d0991dca551d24a
Rajan Jha <138964806+rajanjha9235@users.noreply.github.com>
2024-12-23 20:14:13 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ function Footer() {
       <p>&copy; {currentYear} Rajan Jha</p>
       <div className="flex mt-4 sm:mt-0">
         <a href="https://github.com/rajanjha9235/Sorting_Visualizer" target="_blank" rel="noopener noreferrer">
-          <img src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png" alt="GitHub" className="w-8 h-8 rounded-full" />
+          <img src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png" alt="GitHub" title="Source Code" className="w-8 h-8 rounded-full" />
         </a>
         <a href="https://www.linkedin.com/in/rajanjha9235" target="_blank" rel="noopener noreferrer" className="ml-4">
           <img src="https://upload.wikimedia.org/wikipedia/commons/8/81/LinkedIn_icon.svg" alt="LinkedIn" className="w-8 h-8 rounded-full" />
