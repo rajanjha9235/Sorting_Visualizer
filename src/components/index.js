8a1521e244a64bf0aa2240ce0e85282eeecf6c6b
Rajan Jha <138964806+rajanjha9235@users.noreply.github.com>
2024-12-23 18:59:00 +0530

    Update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -4,6 +4,7 @@ import InsertionSort from "./InsertionSort";
 import SelectionSort from "./SelectionSort";
 import HeapSort from "./HeapSort";
 import QuickSort from "./QuickSort";
+import Footer from "./Footer";
 
 
-export {BubbleSort , MergeSort , SelectionSort , InsertionSort , HeapSort , QuickSort};
+export {BubbleSort , MergeSort , SelectionSort , InsertionSort , HeapSort , QuickSort, Footer};
